a0798951af1856449293ca57fa74e3b3b4ed3157
unknown <wkoppelberger@LT-5CG504391D.c.systemsinmotion.com>
2015-05-13 10:50:51 -0400

    Implemented a MySQL database using Hibernate libraries. Now able to retrieve and send in JSON all Pets inside the DB to the frontend.

--- BackEnd/Pet/src/main/java/org/petfinder/spring/controller/PetRestURIConstants.java
+++ BackEnd/Pet/src/main/java/org/petfinder/spring/controller/PetRestURIConstants.java
@@ -7,5 +7,7 @@ public class PetRestURIConstants {
 	 public static final String GET_ALL_PET = "/pets";
 	 public static final String CREATE_PET = "/create";
 	 public static final String DELETE_PET = "/delete/{id}";
+	 
+	 public static final String CREATE_DATABASE = "/createDB";
 
 }
