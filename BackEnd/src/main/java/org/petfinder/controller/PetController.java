25b5dc56eaa170fa847258e7d451fe2fb338b66b
Bill Koppelberger <wkoppelberger@nexient.com>
2015-05-26 12:57:11 -0400

    Maven dependency set on PetFinder-Core. Makes simple calls to the PetFinderAPI.

--- BackEnd/src/main/java/org/petfinder/controller/PetController.java
+++ BackEnd/src/main/java/org/petfinder/controller/PetController.java
@@ -1,52 +1,57 @@
 package org.petfinder.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
+import org.petfinder.entity.PetfinderPetRecord;
+import org.petfinder.entity.PetfinderPetRecordList;
 import org.petfinder.model.Pet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-//import com.systemsinmotion.petrescue.web.PetFinderConsumer;
+import com.systemsinmotion.petrescue.web.PetFinderConsumer;
 
 
 
-@RestController
+
+
+@Controller
 public class PetController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(PetController.class);
 	
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public @ResponseBody String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		PetFinderConsumer pfc = new PetFinderConsumer();
+		pfc.init();
+		//PetfinderPetRecord pfrff = pfc.randomPet("dog", "pitbull", "L", 'm', "48317", null, null, "formate=json");\
 		
-		String formattedDate = dateFormat.format(date);
 		
-		model.addAttribute("serverTime", formattedDate );
 		
-		return "home";
+		return "petName	: " + pfc.shelterDogs().size();
 	}
 	
 	@RequestMapping(value="/getPet", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
-	public Pet getPet() {
+	
+	public @ResponseBody Pet getPet() {
 		logger.info("getPet");
 		
 		Pet dog1 = new Pet();
 		
-		// Make API call
-		//PetFinderConsumer pfc = new PetFinderConsumer();
-		//PetFinderPetRecord pfr = pfc.findPet(animal, breed, size, sex, location, age, offset, count, output, format);
+		PetFinderConsumer pfc = new PetFinderConsumer();
+		pfc.init();
+		PetfinderPetRecord pfrff = pfc.randomPet(null, null, null, null, null, null, null, null);
 		
+		//dog1.setName(pfrff);
+		System.out.println(pfrff);
 		return dog1;
 	}
 	
