78ff55d7091da47992d63d97a4d63f7962580b96
Bill Koppelberger <wkoppelberger@nexient.com>
2015-05-27 14:49:25 -0400

    Testing changes to petcontroller

--- BackEnd/src/main/java/org/petfinder/controller/PetController.java
+++ BackEnd/src/main/java/org/petfinder/controller/PetController.java
@@ -1,15 +1,20 @@
 package org.petfinder.controller;
 
+import java.awt.List;
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Locale;
 
 import org.petfinder.entity.PetfinderPetRecord;
 import org.petfinder.entity.PetfinderPetRecordList;
-import org.petfinder.model.Pet;
+import org.petfinder.entity.PetfinderShelterRecordList;
+import org.petfinder.model.PetSearchParameters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -31,28 +36,44 @@ public class PetController {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		PetFinderConsumer pfc = new PetFinderConsumer();
-		pfc.init();
-		//PetfinderPetRecord pfrff = pfc.randomPet("dog", "pitbull", "L", 'm', "48317", null, null, "formate=json");\
+		pfc.init();	
 		
+		try {
+			
+			
+			PetfinderPetRecordList findPet = pfc.findPet(null, null, null, null, "48317", null, null, null, null, null);
+			
+			return "" + findPet.getPet().toString();
+			//BigInteger joeyID = pfc.shelterCats().get(0).getId();
+			//return "" + pfc.readPet(joeyID, null).getName();
+		} catch(NullPointerException e) {
+			logger.error("Search results were incorrect!");
+		}
 		
+		//PetfinderShelterRecordList pfpr = pfc.findShelter("48317", null, null, null, "format=json");
+		//pfpr.getShelter().get(0).getAddress1()
 		
-		return "petName	: " + pfc.shelterDogs().size();
+		return "# of Dogs: " + pfc.shelterDogs().size();
 	}
 	
-	@RequestMapping(value="/getPet", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
+	@RequestMapping(value="/tmp", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
 	
-	public @ResponseBody Pet getPet() {
+	public @ResponseBody void getPet() {
 		logger.info("getPet");
-		
-		Pet dog1 = new Pet();
 		
 		PetFinderConsumer pfc = new PetFinderConsumer();
 		pfc.init();
-		PetfinderPetRecord pfrff = pfc.randomPet(null, null, null, null, null, null, null, null);
-		
-		//dog1.setName(pfrff);
-		System.out.println(pfrff);
-		return dog1;
+
 	}
 	
+	@RequestMapping(value="/getPet", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE})
+	public @ResponseBody PetSearchParameters searchPet(@RequestBody PetSearchParameters params) {
+		
+		System.out.println("PARAMETERS: " + params.toString());
+		System.out.println(params.getAnimal() + "-" + params.getAge() + "-");
+		
+		
+		
+		return params;
+	}
 }
