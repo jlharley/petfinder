0028fcacf60212c8b894b232546eee320b650575
Bill Koppelberger <wkoppelberger@nexient.com>
2015-05-27 12:06:01 -0400

    Updated the Pet POJO to match the data/parameters listed in the PetFinder findPet function

--- BackEnd/src/main/java/org/petfinder/model/Pet.java
+++ BackEnd/src/main/java/org/petfinder/model/Pet.java
@@ -1,21 +1,75 @@
 package org.petfinder.model;
 
 public class Pet {
-
-	private String name;
-	private int age;
+	private String animal;
+	private String breed;
+	private String size;
+	private Character sex;
+	private String location;
+	private String age;
+	private Integer offset;
+	private Integer count;
+	private String output;
+	private String format;
 	
-	public String getName() {
-		return name;
+	public String getAnimal() {
+		return animal;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setAnimal(String animal) {
+		this.animal = animal;
 	}
-	public int getAge() {
+	public String getBreed() {
+		return breed;
+	}
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public Character getSex() {
+		return sex;
+	}
+	public void setSex(Character sex) {
+		this.sex = sex;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getAge() {
 		return age;
 	}
-	public void setAge(int age) {
+	public void setAge(String age) {
 		this.age = age;
 	}
-	
+	public Integer getOffset() {
+		return offset;
+	}
+	public void setOffset(Integer offset) {
+		this.offset = offset;
+	}
+	public Integer getCount() {
+		return count;
+	}
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+	public String getOutput() {
+		return output;
+	}
+	public void setOutput(String output) {
+		this.output = output;
+	}
+	public String getFormat() {
+		return format;
+	}
+	public void setFormat(String format) {
+		this.format = format;
+	}	
 }
