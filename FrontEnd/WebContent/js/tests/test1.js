8dbdf52c1ed651256e2ddec2d4e9b6997613afc9
cabaird <cbaird@nexient.com>
2015-05-28 16:54:38 -0400

    indented the test file

--- FrontEnd/WebContent/js/tests/test1.js
+++ FrontEnd/WebContent/js/tests/test1.js
@@ -1,18 +1,16 @@
 describe('Test Example', function() {
-
-beforeEach(module('petFinderApp'));
-
-var mainController,
-scope;
-
-beforeEach(inject(function ($rootScope, $controller) {
-scope = $rootScope.$new();
-mainController = $controller('mainController', {
-$scope: scope
-});
-}));
-it('says hello world!', function () {
-expect(scope.greeting).toEqual("Hello world!");
-});
-
+	beforeEach(module('petFinderApp'));
+	
+	var mainController,
+	scope;
+	
+	beforeEach(inject(function ($rootScope, $controller) {
+		scope = $rootScope.$new();
+		mainController = $controller('mainController', {
+			$scope: scope
+		});
+	}));
+	it('says hello world!', function () {
+		expect(scope.greeting).toEqual("Hello world!");
+	});
 });
