a34889bbe9c03180b2d48be902fc34113a221fa7
cabaird <cbaird@nexient.com>
2015-05-29 13:47:05 -0400

    finished testing nav

--- FrontEnd/WebContent/js/tests/nav.spec.js
+++ FrontEnd/WebContent/js/tests/nav.spec.js
@@ -4,16 +4,20 @@ describe('Nav Controller', function() {
 	
 	beforeEach(inject(function ($rootScope, $controller, $location) {
 		scope = $rootScope.$new();
-		spyOn($location, 'path').andReturn('Fake location');
+		spyOn($location, 'path').and.returnValue('/Fake location');
 		mainController = $controller('navController', {
 			$scope: scope
 		});
 	}));
 	
-	xdescribe('isActive() function', function() {
+	describe('isActive() function', function() {
 		it('returns true if url matches parameter', function () {
-			var viewLocation = "/test";
-			expect(true).toEqual(false);
+			var viewLocation = "/Fake location";
+			expect(scope.isActive(viewLocation)).toEqual(true);
+		});
+		it('returns true if url matches parameter', function () {
+			var viewLocation = "/Fake location/fakey";
+			expect(scope.isActive(viewLocation)).toEqual(false);
 		});
 	});
 });
