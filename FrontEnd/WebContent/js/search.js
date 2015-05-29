e44c770f801ca54a9fc3e429c2012e6cb4f60f4d
cabaird <cbaird@nexient.com>
2015-05-29 15:19:52 -0400

    deleted unused code in search.js and finished testing it

--- FrontEnd/WebContent/js/search.js
+++ FrontEnd/WebContent/js/search.js
@@ -1,11 +1,3 @@
 angular.module('petFinderApp').controller('PetFinderController', function($scope, $http, searchService, $location, user) {
     $scope.pets = searchService;
- 
-    $scope.countPets = function() {
-    	var count = 0;
-    	angular.forEach($scope.pets.results, function(pet) {
-    		count += 1;
-    	});
-    	return count;
-    };
 });
--- FrontEnd/WebContent/js/tests/search.spec.js
+++ FrontEnd/WebContent/js/tests/search.spec.js
@@ -1,24 +1,14 @@
 beforeEach(module('petFinderApp'));
 describe('PetFinderController', function(){
-	var scope, searchService;
-	beforeEach(inject(function($rootScope, _searchService_){
+	var PetFinderController, scope, searchService;
+	beforeEach(inject(function($controller, $rootScope, _searchService_){
 		scope = $rootScope.$new();
 		searchService = _searchService_;
+		PetFinderController = $controller('PetFinderController', {
+			$scope: scope
+		});
 	}));
 	it('should set the scope value of pets', function(){
-		expect(scope.pets).toBeUndefined();
-		scope.pets = searchService;		
 		expect(scope.pets).toBe(searchService);
-	});
-	describe('countPets() function', function(){
-		it('should initialize count to 0', function(){
-			
-		});
-		it('should count pets', function(){
-			
-		});
-		it('return count value', function(){
-			
-		});
 	});
 });
