116d8b0647b1f0f95421e188023111cad89badf6
cabaird <cbaird@nexient.com>
2015-06-01 10:23:55 -0400

    moved the search method into the search controller

--- FrontEnd/WebContent/js/search.js
+++ FrontEnd/WebContent/js/search.js
@@ -1,3 +1,9 @@
 angular.module('petFinderApp').controller('PetFinderController', function($scope, searchService) {
     $scope.pets = searchService;
+    var args;
+    
+    $scope.search = function(){
+    	args = $scope.pets.formatSearchData();
+    	$scope.pets.getPet(args);
+    };
 });
