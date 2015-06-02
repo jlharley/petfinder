3e420381add41d04ff26eca46c57eacc83d8341f
cabaird <cbaird@nexient.com>
2015-06-02 09:49:30 -0400

    added a get random pet function

--- FrontEnd/WebContent/js/search.js
+++ FrontEnd/WebContent/js/search.js
@@ -6,4 +6,45 @@ angular.module('petFinderApp').controller('PetFinderController', function($scope
     	$scope.args = $scope.pets.formatSearchData();  //This will be removed
     	$scope.pets.getPet($scope.args);
     };
+    
+    $scope.getRandomPet = function(){
+    	$scope.pets.getPet({animal:null, breed:null, size:null, sex:null, location:"48108", age:null, offset:null, count:10, output:"full"});
+    };
+    
+    $scope.availableLimits = [1, 5, 10];
+	$scope.limit = 1;
+	
+	$scope.setLimit = function (amount) {
+		$scope.limit = amount;
+	};
+	$scope.isActiveLimit = function (amount) {
+		return amount === $scope.limit;
+	};
+	
+	/*$scope.$watchCollection('[pets.results.length]', function() {
+    	console.log("Change in length:\n\tlength = " + $scope.pets.results.length + "\n\tnumResults = " + $scope.pets.numResults);
+		if ($scope.pets.results.length == 0 && $scope.pets.numResults > 0){
+			$scope.update();
+		}
+	});*/
+	
+	$scope.update = function(){
+		$scope.args = $scope.pets.formatSearchData();  //This will be removed
+    	$scope.pets.getPet($scope.args);
+	};
+	
+	$scope.dislikePet = function(pet){
+		var index = $scope.pets.results.indexOf(pet);
+		$scope.pets.results.splice(index, 1);
+		$scope.pets.numResults --;
+	};
+	
+	$scope.likePet = function(pet){
+		var index = $scope.pets.results.indexOf(pet);
+		$scope.pets.results.splice(index, 1);
+		$scope.pets.numResults --;
+	};
+	
+    $scope.getRandomPet();
+    
 });
