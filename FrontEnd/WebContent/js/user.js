cef4f8bb4fe02a8fd90ca7bfcacd926abfb005ce
cabaird <cbaird@nexient.com>
2015-06-02 16:49:39 -0400

    created test for the login module which is empty for now

--- FrontEnd/WebContent/js/user.js
+++ FrontEnd/WebContent/js/user.js
@@ -4,32 +4,4 @@ angular.module('petFinderApp').service('user', function(){
 	user.firstName = "";
 	user.lastName = "";
 	user.nickname = "";
-
-	user.getUsername = function() {
-        return user.username;
-    };
-    user.getFirstName = function() {
-        return user.firstName;
-    };
-    user.getLastName = function() {
-        return user.lastName;
-    };
-    user.getNickname = function() {
-        return user.nickname;
-    };
-    user.getFullName = function() {
-        return user.firstName + " " + user.lastName;
-    };
-    user.setUsername = function(name) {
-    	user.username = name;
-    };
-    user.setFirstName = function(name) {
-    	user.firstName = name;
-    };
-    user.setLastName = function(name) {
-    	user.lastName = name;
-    };
-    user.setNickname = function(name) {
-    	user.nickname = name;
-    };
 });
