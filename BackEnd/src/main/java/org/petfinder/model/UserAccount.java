6ba31fac2142c78f902ac37d7da936a8d11e8f6e
Bill Koppelberger <wkoppelberger@nexient.com>
2015-06-02 16:37:15 -0400

    created UserAccount POJO

---
+++ BackEnd/src/main/java/org/petfinder/model/UserAccount.java
@@ -0,0 +1,55 @@
+package org.petfinder.model;
+
+/**
+ * @author wkoppelberger
+ *
+ */
+public class UserAccount {
+
+	private String firstName;
+	private String lastName;
+	private String emailAddress;
+	private String address;
+	private String zipcode;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+}
