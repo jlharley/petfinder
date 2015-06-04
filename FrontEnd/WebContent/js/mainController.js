1bf29146d7725c638a615639636ad4d7bd74035b
cabaird <cbaird@nexient.com>
2015-06-04 13:10:52 -0400

    added getUserInfo

--- FrontEnd/WebContent/js/mainController.js
+++ FrontEnd/WebContent/js/mainController.js
@@ -1,3 +1,36 @@
 angular.module('petFinderApp').controller('mainController', function($scope, user) {
 	$scope.user = user;
+	$scope.userInfo;
+	
+	$scope.$on('event:google-plus-signin-success', function (event,authResult) {
+			console.log('Signed in!');
+			console.log(authResult);
+			$scope.getUserInfo();
+	  });
+	  $scope.$on('event:google-plus-signin-failure', function (event,authResult) {
+		  console.log('Not signed into Google Plus.');
+	  });
+	  $scope.signOut = function(){
+		  console.log("signing out");
+		  gapi.auth.signOut();
+	        $scope.$apply;
+	  }
+	 
+	// When callback is received, process user info.
+	$scope.userInfoCallback = function(userInfo) {
+	    $scope.$apply(function() {
+	        console.log("user:" + JSON.stringify(userInfo));
+	        $scope.userInfo = userInfo;
+	        $scope.$apply;
+	    });
+	};
+	  $scope.getUserInfo = function() {
+		    gapi.client.request(
+		        {
+		            'path':'/plus/v1/people/me',
+		            'method':'GET',
+		            'callback': $scope.userInfoCallback
+		        }
+		    );
+		};
 });
