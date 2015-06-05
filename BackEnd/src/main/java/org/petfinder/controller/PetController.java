7ca7bdb6629eeecf8e726b881189c50f430b201a
Bill Koppelberger <wkoppelberger@nexient.com>
2015-06-05 12:56:40 -0400

    getLikedPets functionality implemented

--- BackEnd/src/main/java/org/petfinder/controller/PetController.java
+++ BackEnd/src/main/java/org/petfinder/controller/PetController.java
@@ -1,8 +1,10 @@
 package org.petfinder.controller;
 
+import java.math.BigInteger;
 import java.util.List;
 import java.util.Locale;
 
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -12,6 +14,7 @@ import org.petfinder.entity.PetfinderPetRecord;
 import org.petfinder.entity.PetfinderPetRecordList;
 import org.petfinder.model.PetSearchParameters;
 import org.petfinder.model.UserAccount;
+import org.petfinder.model.UserDislikedPet;
 import org.petfinder.model.UserLikedPet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,6 +36,7 @@ public class PetController {
 	private static SessionFactory sessionFactory;
 	private static ServiceRegistry serviceRegistry;
 
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public @ResponseBody String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
@@ -41,7 +45,7 @@ public class PetController {
 		pfc.init();	
 		
 		try {
-			PetfinderPetRecordList findPet = pfc.findPet(null, null, null, null, "48317", null, null, 1, null, null);
+			PetfinderPetRecordList findPet = pfc.findPet(null, null, null, null, "48317", null, null, 5, null, null);
 			return "" + findPet.getPet().toString();
 		} catch(NullPointerException e) {
 			logger.error("Search results were incorrect!");
@@ -67,7 +71,7 @@ public class PetController {
 		
 		return petList;
 		}catch(NullPointerException e) {
-			logger.error("Search results were incorrect!");
+			logger.error("Search results were incorrect!: " + e);
 		}
 		
 		return null;
@@ -79,76 +83,140 @@ public class PetController {
 	    System.out.println("Hibernate Annotation Configuration loaded");
 	    
 	    serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
-	    //serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
 	    System.out.println("Hibernate Java Config serviceRegistry created");
 	    
 	    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
 	    return sessionFactory;
 	}
 	
-	/*@RequestMapping(value = "/initDB")
-	private @ResponseBody void initDB() {
-		
-		// Contains all the data regarding the hibernate config file
-		SessionFactory sessionFactory = createSessionFactory();
-				
-		// Opens a database connection
-		Session session = sessionFactory.openSession();
-		
-		UserAccount bob = new UserAccount();
-		bob.setEmailAddress("bob@gmail.com");
-		
-		// Database usage 
-		session.beginTransaction();
-		session.save(bob);
-		session.getTransaction().commit();
-		session.close();
-		
-		// Closes a database connection
-		sessionFactory.close();
-	}*/
-	
 	@RequestMapping(value = "/addUserDB", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE})
 	public @ResponseBody void addUserDB(@RequestBody UserAccount account) {
-
-		try {
-		SessionFactory sessionFactory = createSessionFactory();
-		Session session = sessionFactory.openSession();
-
-		session.beginTransaction();
-		session.save(account);
-		session.getTransaction().commit();
-		session.close();
-		sessionFactory.close();
 		
-		logger.debug("User has been added to the DB!");
-		}catch(Exception e) {
-			logger.debug("Failed to add user to the Database!\n" + e);
-		}
-		
-	}
-	
-	@RequestMapping(value = "/addLikedPet", method = RequestMethod.GET)
-	public @ResponseBody void addLikedPet(/*@RequestBody UserPreferenceData likedPet*/) {
+		System.out.println("EMAIL:" + account.getEmailAddress());
 		
 		try {
 			SessionFactory sessionFactory = createSessionFactory();
 			Session session = sessionFactory.openSession();
-
-			UserLikedPet data = new UserLikedPet();
-			data.setEmailAddress("asdfadf@asdefaf.com");
-			data.setPetId(11112);
-			
-			
+	
 			session.beginTransaction();
-			session.save(data);
+			session.save(account);
 			session.getTransaction().commit();
 			session.close();
 			sessionFactory.close();
 			
 			logger.debug("User has been added to the DB!");
+		}catch(Exception e) {
+			logger.debug("Failed to add user to the Database!\n" + e);
+			//close session if failed??
+		}
+		
+	}
+	
+	@RequestMapping(value = "/addLikedPet", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE})
+	public @ResponseBody void addLikedPet(@RequestBody UserLikedPet likedPet) {
+		
+		try {
+			SessionFactory sessionFactory = createSessionFactory();
+			Session session = sessionFactory.openSession();
+
+			System.out.println("EMAIL: " + likedPet.getEmailAddress());
+			
+			session.beginTransaction();
+			session.save(likedPet);
+			session.getTransaction().commit();
+			session.close();
+			sessionFactory.close();
+			
+			logger.debug("Liked Pet has been added to the DB!");
 			}catch(Exception e) {
-				logger.debug("User has been added to the DB!");
+				logger.debug("Failed to add Liked Pet to the Database!\n" + e);
+				//close session if failed??
 			}
+	}
+	
+	@RequestMapping(value = "/addDislikedPet", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE})
+	public @ResponseBody void addDislikedPet(@RequestBody UserDislikedPet dislikedPet) {
+		
+		try {
+			SessionFactory sessionFactory = createSessionFactory();
+			Session session = sessionFactory.openSession();
+			
+
+			session.close();
+			sessionFactory.close();
+			
+			logger.debug("Disliked Pet has been added to the DB!");
+			}catch(Exception e) {
+				logger.debug("Failed to add Disliked Pet to the Database!\n" + e);
+				//close session if failed??
+			}
+	}
+	
+	@RequestMapping(value = "/getLikedPets", method = RequestMethod.GET/*, consumes = {MediaType.APPLICATION_JSON_VALUE}*/)
+	public @ResponseBody List<PetfinderPetRecord> getLikedPets(/*@RequestBody UserLikedPet user*/) {
+		logger.debug("Retrieving Liked Pets from the DB");
+		
+		PetfinderPetRecordList likedPetRecords = new PetfinderPetRecordList();
+		PetFinderConsumer pfc = new PetFinderConsumer();
+		pfc.init();	
+		
+		try {
+			SessionFactory sessionFactory = createSessionFactory();
+			Session session = sessionFactory.openSession();
+
+			// Queries the DB for the user's liked Pets
+			SQLQuery query = session.createSQLQuery("select petId from user_liked where emailAddress='bartman@gmail.com'");
+			System.out.println("Query: " + query.list().toString());
+
+			// Retrieves the PetfinderPetRecords from the PetFinder API of the PetIDs retrieved from the DB
+			for(int i = 0; i < query.list().size(); i++) {
+				PetfinderPetRecord tmpRecord = pfc.readPet(BigInteger.valueOf((Integer)query.list().get(i)), null);
+				likedPetRecords.getPet().add(tmpRecord);
+			}
+			
+			session.close();
+			sessionFactory.close();
+			
+			return likedPetRecords.getPet();
+		}catch(NullPointerException e) {
+			logger.error("Search results were incorrect!: " + e);
+			//close session if failed??
+		}
+		
+		return null;
+	}
+	
+	@RequestMapping(value = "/getDislikedPets", method = RequestMethod.GET/*, consumes = {MediaType.APPLICATION_JSON_VALUE}*/)
+	public @ResponseBody List<PetfinderPetRecord> getDislikedPets(/*@RequestBody UserLikedPet user*/) {
+		logger.debug("Retrieving Disliked Pets from the DB");
+		
+		PetfinderPetRecordList dislikedPetRecords = new PetfinderPetRecordList();
+		PetFinderConsumer pfc = new PetFinderConsumer();
+		pfc.init();	
+		
+		try {
+			SessionFactory sessionFactory = createSessionFactory();
+			Session session = sessionFactory.openSession();
+
+			// Queries the DB for the user's liked Pets
+			SQLQuery query = session.createSQLQuery("select petId from user_disliked where emailAddress='bartman@gmail.com'");
+			System.out.println("Query: " + query.list().toString());
+
+			// Retrieves the PetfinderPetRecords from the PetFinder API of the PetIDs retrieved from the DB
+			for(int i = 0; i < query.list().size(); i++) {
+				PetfinderPetRecord tmpRecord = pfc.readPet(BigInteger.valueOf((Integer)query.list().get(i)), null);
+				dislikedPetRecords.getPet().add(tmpRecord);
+			}
+			
+			session.close();
+			sessionFactory.close();
+			
+			return dislikedPetRecords.getPet();
+		}catch(NullPointerException e) {
+			logger.error("Search results were incorrect!: " + e);
+			//close session if failed??
+		}
+		
+		return null;
 	}
 }
